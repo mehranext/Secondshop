5a88d0594718de36188c2f1e40f74d589d317a43
Mehranxt <132151347+mehranext@users.noreply.github.com>
2024-01-13 09:20:26 +0330

    lets comment what we need and write it.

--- secondshop/lib/main.dart
+++ secondshop/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:secondshop/app.dart';
+import 'package:secondshop/utils/constants/colors.dart';
 
+
+///-------- Main Function is just  here & its Entry point of Flutter App -------
 void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  // Todo : Add Widgets Bindings
+  // Todo : Init Local Storage
+  // Todo : Await Native Splash
+  // Todo : Initialize Firebase
+  // Todo : Initialize Authentication
+///(inside the main function we add this)///
+  runApp(const App());
 }
