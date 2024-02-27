f6688514706a3cb88ba0da7ea248f7e530637c00
Mehranxt <132151347+mehranext@users.noreply.github.com>
2024-02-27 11:04:24 +0330

    otherwise in some pages and locations in app we have a constant texts which they R so important
    and in here we set it in the text string file that we created before.

---
+++ secondshop/lib/utils/constants/text_string.dart
@@ -0,0 +1,20 @@
+ /// This Class contains all the App Text in String formats.
+
+ class TTexts{
+
+  // -- GLOBAL Texts
+
+  // -- onBoarding Texts
+  static const String onBoardingTitle1 = "Choose Your Product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at Your door step";
+
+  static const String onBoardingSubTitle1 = "Welcome to a world of Limitless Choices - Your Perfect Product Awaits! ";
+  static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBoardingSubTitle3= "From Our Doorstep to Yours - swift , Secure , and Contactless Delivery !";
+
+
+  // --Home
+  static const String homeAppbarTitles = "Good Day For Shopping";
+  static const String homeAppbarSubTitle = "What Are You Waiting For";
+ }
