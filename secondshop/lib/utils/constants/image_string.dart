bebb66c274ffac6a6443f028dbaa4f6cdce96c46
Mehranxt <132151347+mehranext@users.noreply.github.com>
2024-02-27 11:01:13 +0330

    Lets fix Image string file for that time that we need it in our app just we call a simple word and work with it.(add some pictures)

---
+++ secondshop/lib/utils/constants/image_string.dart
@@ -0,0 +1,14 @@
+
+/// This class contains all the app Images in String Formats.
+/// and we can call it simple like darkAPPLogo or lightAppLogo
+class TImages {
+
+// -- App Logos
+  static const String darkAppLogo = "assets/logos/blackSplash.png";
+  static const String lightAppLogo = "assets/logos/witheSplash.png";
+
+  // -- Social Logos
+  static const String google = "assets/logos/icons8-google-48.png";
+  static const String facebook = "assets/logos/icons8-facebook-48.png";
+
+}
