d93b3e7163077c13b140d0b434f28b57d42458be
Mehranxt <132151347+mehranext@users.noreply.github.com>
2024-02-27 11:24:50 +0330

    yes the enums file that we just created for now and after we have work more in this file for now just a simple TEXT SIZE AND ORDER STATUS AND PAYMENT METHOD that we have.

---
+++ secondshop/lib/utils/constants/enums.dart
@@ -0,0 +1,10 @@
+
+/// List of Enums
+/// They can't be created inside a class.
+
+
+enum TextSizes {small , medium, large }
+
+enum OrderStatus {processing , shipped , delivered}
+
+enum PaymentMethod {paypal , googlePay, applePay,visa,masterCard,creditCard,giftCard, razorPay,paytm }
